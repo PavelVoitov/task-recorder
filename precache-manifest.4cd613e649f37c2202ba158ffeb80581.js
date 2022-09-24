7b613c6a458fb97a6dd9710e8693d37e23d085d3
PavelVoitov <voitov.pavel@yandex.ru>
2022-09-25 00:40:06 +0300

    Updates

---
+++ precache-manifest.4cd613e649f37c2202ba158ffeb80581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fea8eb2ec61c41a07710d91dc308969",
+    "url": "/it-incubator-todolist-ts-01/index.html"
+  },
+  {
+    "revision": "9f863cec85db50e389d7",
+    "url": "/it-incubator-todolist-ts-01/static/css/main.381040d6.chunk.css"
+  },
+  {
+    "revision": "d3c73afadb7307723db6",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.4c5467d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.4c5467d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f863cec85db50e389d7",
+    "url": "/it-incubator-todolist-ts-01/static/js/main.6306b909.chunk.js"
+  },
+  {
+    "revision": "f66ab8bd731da1733e57",
+    "url": "/it-incubator-todolist-ts-01/static/js/runtime-main.fae40ad6.js"
+  }
+]);
